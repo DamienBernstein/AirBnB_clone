4a0a9cebad5ca30253064e99f0ad6d2c85b648bf
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-07 14:13:24 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -9,46 +9,56 @@ import models
 
 
 class BaseModel:
-    """ class for all other classes to inherit from """
+    """ 
+    Base class for other classes to inherit from 
+    """
     def __init__(self, *args, **kwargs):
-        """ Constructor and re-create an instance with
-        this dictionary representation"""
-        if len(kwargs) > 0:
-            # each key of this dictionary is an attribute name
-            # each value of this dictionary is the value of this attribute name
-            for key, value in kwargs.items():
-                if key == "updated_at":
-                    # Convert string date to datetime object
-                    # strptime (string parse time): Parse a string into a -
-                    # datetime object given a corresponding format
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "created_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "__class__":
-                    # This happens because __class__ is not mandatory in output
-                    continue
-
-                setattr(self, key, value)
-        else:
-            # Generate a random UUID
-            self.id = str(uuid.uuid4())
-            # assign with the current datetime when an instance is created
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            # if it’s a new instance add a call to the method new(self) on stge
-            models.storage.new(self)
-
-    def __str__(self):
-        """ overriding the __str__ method that returns a custom
-        string object """
-        # Old-style: self.__class__.__name__
-        class_name = type(self).__name__
-        mssg = "[{0}] ({1}) {2}".format(class_name, self.id, self.__dict__)
-        return (mssg)
-
-    # Public instance methods
-    def save(self):
-        """ updates the public instance attribute updated_at with
-        the current datetime """
+        # Generate a random UUID for each instance
+        self.id = str(uuid.uuid4())
+        # Set the created_at time to the current time
+        self.created_at = datetime.now()
+        # Set the updated_at time to the current time
         self.updated_at = datetime.now()
-        models.storage.save()
+
+        # If kwargs is not empty (i.e. there are additional attributes)
+        if kwargs:
+            # Call the helper function to recreate the instance from the kwargs
+            self._recreate_from_dict(kwargs)
+            # Add the instance to the storage
+            models.storage.new(self)
+    
+    def _recreate_from_dict(self, data):
+        # Iterate through each key-value pair in the data dictionary
+        for key, value in data.items():
+            # If the key is "updated_at", parse the string into a datetime object
+            if key == "updated_at":
+                value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+            # If the key is "created_at", parse the string into a datetime object
+            elif key == "created_at":
+                value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+            # If the key is "__class__", skip it
+            elif key == "__class__":
+                continue
+
+            # Set the value of the attribute with the given key
+            setattr(self, key, value)
+
+  def __str__(self):
+    """
+    Overriding the built-in `__str__` method to return a custom string representation
+    of the object
+    """
+    # Get the name of the class as a string
+    class_name = type(self).__name__
+    # Use an f-string to build the string representation
+    return f"[{class_name}] ({self.id}) {self.__dict__}"
+
+
+   def save(self):
+    """
+    Save the current state of the object to the file
+    """
+    # Update the `updated_at` attribute with the current datetime
+    self.updated_at = datetime.now()
+    # Call the save method of the storage object to persist the changes
+    models.storage.save()
