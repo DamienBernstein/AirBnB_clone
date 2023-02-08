14d8e4406364a2eea9effa36f44cbcc83450117c
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-08 15:03:16 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -5,8 +5,8 @@ from . base_model import BaseModel
 
 class User(BaseModel):
       """ class User that inherits from BaseModel """
-      first_name = ' '
-      Last_name = ' '
-      email = ' '
-      password = ' '
+      first_name = ''
+      Last_name = ''
+      email = ''
+      password = ''
       
