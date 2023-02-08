10397ec4699f52dd6c914774ccd0a4da5fb5441c
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-08 14:43:17 +0200

    Create city.py

---
+++ models/city.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" City where user's come from """
+from . base_model import BaseModel
+
+class City(BaseModel):
+      """ city of the user """ 
+      name = ' '
+      state_id = ' ' 
