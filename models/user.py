21b63483db56513e8aa719bf13db242f632a3d19
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-08 14:27:18 +0200

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" First User in AirBnB Project """
+from . base_model import BaseModel
+
+class User(BaseModel):
+      """ class User that inherits from BaseModel """
+      first_name = ' '
+      Last_name = ' '
+      email = ' '
+      password = ' '
+      
