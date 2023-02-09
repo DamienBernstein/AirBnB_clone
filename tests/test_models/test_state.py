7d877420678bdf5a2dd6bc82a0501a52c7e4c46a
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-09 10:12:40 +0200

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -9,42 +9,68 @@ from models.user import User
 from models.base_model import BaseModel
 
 
-class test_Amenity(unittest.TestCase):
+class TestAmenity(unittest.TestCase):
+    """
+    TestAmenity class contains all the test cases for the Amenity class
+    """
 
     @classmethod
-    def setup(self):
-        self.state1 = State()
-        self.state1.name = " "
+    def setUpClass(cls):
+        """
+        This method creates an instance of the Amenity class for testing
+        """
+        cls.state1 = State()
+        cls.state1.name = "los Durba"
 
     @classmethod
-    def tearDown(self):
-        del self.state1
+    def tearDownClass(cls):
+        """
+        This method deletes the instance of the Amenity class and removes the file created during testing
+        """
+        del cls.state1
         try:
             os.remove("file.json")
         except FileNotFoundError:
             pass
 
     def test_to_dict(self):
+        """
+        This method tests if the to_dict method exists in the Amenity class
+        """
         self.assertEqual("to_dict" in dir(self.state1), True)
 
     def test_functions(self):
+        """
+        This method tests if the __doc__ string exists for the Amenity class
+        """
         self.assertIsNotNone(State.__doc__)
 
-    def save_test(self):
+    def test_save(self):
+        """
+        This method tests if the save method updates the created_at and updated_at attributes correctly
+        """
         self.state1.save()
-        self.assertNotEqual(self.state1.created_at,
-                            self.state1.updated_at)
+        self.assertNotEqual(self.state1.created_at, self.state1.updated_at)
 
     def test_subclass(self):
-        self.assertTrue(issubclass(self.state1.__class__.BaseModel), True)
+        """
+        This method tests if the Amenity class is a subclass of BaseModel
+        """
+        self.assertTrue(issubclass(self.state1.__class__, BaseModel), True)
 
     def test_attributes(self):
+        """
+        This method tests if the Amenity class has all the required attributes
+        """
         self.assertTrue("name" in self.state1.__dict__)
         self.assertTrue("created_at" in self.state1.__dict__)
         self.assertTrue("updated_at" in self.state1.__dict__)
         self.assertTrue("id" in self.state1.__dict__)
 
     def test_strings(self):
+        """
+        This method tests if the name attribute is a string
+        """
         self.assertEqual(type(self.state1.name), str)
 
 if __name__ == "__main__":
