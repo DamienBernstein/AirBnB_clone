6321be972318a8e7f8e5ffc2bc8e3e6677860925
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-11 14:08:54 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" City where user's come from """
-from . base_model import BaseModel
+"""City where users come from"""
+from .base_model import BaseModel
 
 class City(BaseModel):
-      """ city of the user """ 
-      name = ''
-      state_id = '' 
+"""City of the user"""
+name = ""
+state_id = ""
