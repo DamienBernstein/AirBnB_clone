d3276a398c27b213dc5fba64e7322284e3b022f2
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-11 14:23:45 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
 """ First User in AirBnB Project """
-from . base_model import BaseModel
+from .base_model import BaseModel
+
 
 class User(BaseModel):
-      """ class User that inherits from BaseModel """
-      first_name = ''
-      Last_name = ''
-      email = ''
-      password = ''
+    """ class User that inherits from BaseModel """
+    first_name = ''
+    Last_name = ''
+    email = ''
+    password = ''
