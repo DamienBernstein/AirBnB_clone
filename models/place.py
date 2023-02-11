0de6f5622cefc493f61b3edd6525dae99576e8c9
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-11 14:18:51 +0200

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-""" place of the user """
-from . base_model import BaseModel
+"""Place of the user"""
+from .base_model import BaseModel
+
 
 class Place(BaseModel):
-      """ place of user """
-      city_id = ''
-      user_id = ''
-      name = ''
-      description = '' 
-      number_rooms = 0
-      number_bedrooms = 0
-      max_guests = 0
-      price_by_night= 0
-      latitude = 0.0
-      longitude = 0.0
-      amendity_ids = []
+    """Place of the user"""
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bedrooms = 0
+    max_guests = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
