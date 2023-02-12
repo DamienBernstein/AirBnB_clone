a5eea424e6c03d2658de9aa4d4fb61c5ad4e5b49
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-13 00:01:46 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,48 +12,42 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
-
 class FileStorage:
-    """ serializes instances to a JSON file and deserializes
-    JSON file to instances """
     __file_path = "file.json"
     __objects = {}
-    lavel_dict = {"BaseModel": BaseModel, "User": User, "Amenity": Amenity,
-                  "City": City, "Place": Place, "State": State,
-                  "Review": Review}
 
     def all(self):
-        """ returns the dictionary __objects """
-        return (self.__objects)
+        return FileStorage.__objects
 
     def new(self, obj):
-        """ sets in __objects the obj with key <obj class name>.id """
-        if obj:
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            self.__objects[key] = obj
+        FileStorage.__objects[obj.__class__.__name__ + "." + obj.id] = obj
 
     def save(self):
-        """ serializes __objects to the JSON file (path: __file_path) """
-        ser_dict = {}
-        all_dict = FileStorage.__objects
         with open(FileStorage.__file_path, 'w') as f:
-            for value in all_dict.values():
-                key = "{}.{}".format(value.__class__.__name__, value.id)
-                ser_dict[key] = value.to_dict()
-            json.dump(ser_dict, f)
+            obj_dict = {}
+            for key, value in FileStorage.__objects.items():
+                obj_dict[key] = value.to_dict()
+            f.write(json.dumps(obj_dict))
 
     def reload(self):
-        """ deserializes the JSON file to __objects (only if the JSON file
-        (__file_path) exists, otherwise, do nothing. If the file doesn’t
-        exist, no exception should be raised) """
-        # Validate if file exists
-        if os.path.isfile(self.__file_path):
-            with open(self.__file_path, 'r') as f:
-                des_json = json.load(f)
-                for key, value in des_json.items():
-                    # Separate name_class from id and split the separator
-                    k = key.split('.')
-                    # search "__class__": "BaseModel"
-                    class_name = k[0]
-                    # set in __objects the key, value
-                    self.new(eval("{}".format(class_name))(**value))
+        try:
+            with open(FileStorage.__file_path, 'r') as f:
+                obj_dict = json.loads(f.read())
+                for key, value in obj_dict.items():
+                    class_name, obj_id = key.split(".")
+                    if class_name == "BaseModel":
+                        FileStorage.__objects[key] = BaseModel(**value)
+                    elif class_name == "User":
+                        FileStorage.__objects[key] = User(**value)
+                    elif class_name == "Place":
+                        FileStorage.__objects[key] = Place(**value)
+                    elif class_name == "State":
+                        FileStorage.__objects[key] = State(**value)
+                    elif class_name == "City":
+                        FileStorage.__objects[key] = City(**value)
+                    elif class_name == "Amenity":
+                        FileStorage.__objects[key] = Amenity(**value)
+                    elif class_name == "Review":
+                        FileStorage.__objects[key] = Review(**value)
+        except:
+            pass
