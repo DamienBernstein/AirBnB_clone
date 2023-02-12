94cb0e31981ed2fb0d83de7ad8daf4944f690382
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-12 22:58:02 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,6 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
-        storage = FileStorage()
         storage.new(self)
 
     def _recreate_from_dict(self, data):
