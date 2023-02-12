3fd2750024bfbdd646817202d0602ba7dc4a9276
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-12 22:56:47 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -6,20 +6,15 @@ import uuid
 from datetime import datetime
 # import the variable storage
 import models
-
+from models.engine.file_storage import FileStorage
 
 class BaseModel:
-    """Base class for other classes to inherit from."""
     def __init__(self, *args, **kwargs):
-        """Initializes a new instance of the class."""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        models.storage.new(self)
-
-        if kwargs:
-            self._recreate_from_dict(kwargs)
-            models.storage.new(self)
+        self.updated_at = self.created_at
+        storage = FileStorage()
+        storage.new(self)
 
     def _recreate_from_dict(self, data):
         """Helper function to recreate the instance from a dictionary."""
