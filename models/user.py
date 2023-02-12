a6173761a028c0bd8e3ef0cbea49d4817113f6fe
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-13 00:09:04 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -13,14 +13,3 @@ class User(BaseModel):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-    def __str__(self):
-        return "[User] ({}) {}".format(self.id, self.__dict__)
-
-    def to_dict(self):
-        user_dict = super().to_dict()
-        user_dict["email"] = self.email
-        user_dict["password"] = self.password
-        user_dict["first_name"] = self.first_name
-        user_dict["last_name"] = self.last_name
-        return user_dict
