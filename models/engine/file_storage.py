09d2584b015a52d64c208f6acd33e6c73ef83e38
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-12 21:09:45 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,6 +10,13 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
+# models/engine/file_storage.py
+import json
+from models.base_model import BaseModel
+from models.user import User
+
+
+
 
 class FileStorage:
     FILE_PATH = "file.json"
@@ -52,3 +59,30 @@ class FileStorage:
                         obj = cls(**value)
                         obj.id = obj_id
                         self.new(obj)
+                        
+ 
+  def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        FileStorage.__objects[obj.__class__.__name__ + "." + obj.id] = obj
+
+    def save(self):
+        with open(FileStorage.__file_path, 'w') as f:
+            obj_dict = {}
+            for key, value in FileStorage.__objects.items():
+                obj_dict[key] = value.to_dict()
+            f.write(json.dumps(obj_dict))
+
+    def reload(self):
+        try:
+            with open(FileStorage.__file_path, 'r') as f:
+                obj_dict = json.loads(f.read())
+                for key, value in obj_dict.items():
+                    class_name, obj_id = key.split(".")
+                    if class_name == "BaseModel":
+                        FileStorage.__objects[key] = BaseModel(**value)
+                    elif class_name == "User":
+                        FileStorage.__objects[key] = User(**value)
+        except:
+            pass
