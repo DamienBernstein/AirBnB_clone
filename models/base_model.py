43c179488128beacd9e9a333f891b326b98a187f
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-12 21:44:38 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -15,6 +15,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        models.storage.new(self)
 
         if kwargs:
             self._recreate_from_dict(kwargs)
