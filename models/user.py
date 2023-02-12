b2ac085b5f9241c9a4713d218bf5712f90522f2b
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-12 21:07:31 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -9,3 +9,22 @@ class User(BaseModel):
     Last_name = ''
     email = ''
     password = ''
+
+    # console.py
+from models.base_model import BaseModel
+from models.user import User
+from models.engine.file_storage import FileStorage
+
+class Console:
+    def do_create(self, line):
+        if line == "BaseModel":
+            new_instance = BaseModel()
+        elif line == "User":
+            new_instance = User()
+        new_instance.save()
+        print(new_instance.id)
+
+    def do_show(self, line):
+        try:
+            class_name, obj_id = line.split()
+            obj = storage.
